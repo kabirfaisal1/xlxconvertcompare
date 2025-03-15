6d669edd44ba854378021d49f623b9a2b63eca3e
kabirfaisal1 <md.kabirfaisal@outlook.com>
2025-03-15 10:56:37 -0400

    added page nav

--- components/navigation-menu/main-nav.jsx
+++ components/navigation-menu/main-nav.jsx
@@ -2,7 +2,7 @@
 
 // Global imports
 import { cn } from "@/lib/utils";
-import { usePathname, useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 // Local imports
 import
@@ -14,38 +14,43 @@ import
 
 export function MainNav ( { className, ...props } )
 {
-  const pathname = usePathname();
-  const params = useParams();
+  const pathname = usePathname(); // Directly use usePathname() to prevent hydration issues
 
   /**
    * Navigation routes for the application
    */
   const routes = [
     {
-      href: `/excelConverter`,
-      label: "Excel Converter",
-      active: pathname === `/excelConverter`,
+      href: `/excelConverter`, // Ensure lowercase for consistency
+      label: "Convert Excel to Code",
+      active: pathname === `/excelConverter`, // Match the exact path
       id: "excel_Converter",
+    },
+    {
+      href: `/jsonConverter`, // Ensure lowercase for consistency
+      label: "Convert JSON to Excel",
+      active: pathname === `/jsonConverter`, // Match the exact path
+      id: "json_Converter",
     },
   ];
 
   return (
     <NavigationMenu
-      className={cn( "flex items-center space-x-4", className )}
+      className={cn( "flex items-center space-x-2", className )}
       data-testid="main-NavigationMenu"
       {...props}
     >
-      <NavigationMenuList className="flex space-x-4">
+      <NavigationMenuList className="flex space-x-2">
         {routes.map( ( route ) => (
           <NavigationMenuLink
             key={route.href}
             href={route.href}
             data-testid={route.id}
             className={cn(
-              "pt-4 text-sm font-medium transition-colors hover:text-primary",
+              "relative px-4 py-2 text-sm font-medium transition-colors rounded-t-lg border-b-2 border-transparent bg-gray-100 dark:bg-gray-800",
               route.active
-                ? "text-black dark:text-white"
-                : "text-muted-foreground"
+                ? "bg-white dark:bg-gray-900 border-b-white dark:border-b-gray-900 text-black dark:text-white shadow-[0px_4px_10px_#8B5DFF]"
+                : "text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700"
             )}
           >
             {route.label}
