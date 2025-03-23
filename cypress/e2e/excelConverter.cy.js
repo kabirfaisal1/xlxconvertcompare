5a8f4539fce908c44178cb9ebe7a4c6c7500b358
kabirfaisal1 <md.kabirfaisal@outlook.com>
2025-03-23 18:03:37 -0400

    updated TC

--- cypress/e2e/excelConverter.cy.js
+++ cypress/e2e/excelConverter.cy.js
@@ -12,7 +12,7 @@ describe( 'Excel File Uploader & Converter', () =>
         cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'JSVariable' ).should( 'have.value', 'JSVariable' );
         cy.get( 'button[role="combobox"]' ).click();
         cy.get( 'select' ).select( 'JavaScript', { force: true } );
-        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'JavaSript' );
+        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'JavaScript' );
         // Upload the Excel file
         cy.get( 'input[type="file"]' ).selectFile( 'cypress/fixtures/2Game_of_Thrones_Cast_List_with_Earnings.xlsx', { force: true } );
         cy.get( '[data-testid="totalEntries_count"]' ).should( 'be.visible' );
@@ -20,10 +20,10 @@ describe( 'Excel File Uploader & Converter', () =>
     } );
     it( 'Upload Excel and Convert to JSON [C7]', () =>
     {
-        cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'JSONVariable' ).should( 'have.value', 'Json' );
+        cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'JSONVariable' ).should( 'have.value', 'JSONVariable' );
         cy.get( 'button[role="combobox"]' ).click();
-        cy.get( 'select' ).select( 'Json', { force: true } );
-        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'Json' );
+        cy.get( 'select' ).select( 'JSON', { force: true } );
+        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'JSON' );
         // Upload the Excel file
         cy.get( 'input[type="file"]' ).selectFile( 'cypress/fixtures/2Game_of_Thrones_Cast_List_with_Earnings.xlsx', { force: true } );
         cy.get( '[data-testid="totalEntries_count"]' ).should( 'be.visible' );
@@ -32,7 +32,7 @@ describe( 'Excel File Uploader & Converter', () =>
     } );
     it( 'Upload Excel and Convert to C# [C4]', () =>
     {
-        cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'C#Variable' ).should( 'have.value', 'C#Variable' );
+        cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'CSharpVariable' ).should( 'have.value', 'CSharpVariable' );
         cy.get( 'button[role="combobox"]' ).click();
         cy.get( 'select' ).select( 'C#', { force: true } );
         cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'C#' );
@@ -45,8 +45,8 @@ describe( 'Excel File Uploader & Converter', () =>
     {
         cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'TSVariable' ).should( 'have.value', 'TSVariable' );
         cy.get( 'button[role="combobox"]' ).click();
-        cy.get( 'select' ).select( 'TypeScript#', { force: true } );
-        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'TypeScript#' );
+        cy.get( 'select' ).select( 'TypeScript', { force: true } );
+        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'TypeScript' );
         // Upload the Excel file
         cy.get( 'input[type="file"]' ).selectFile( 'cypress/fixtures/2Game_of_Thrones_Cast_List_with_Earnings.xlsx', { force: true } );
         cy.get( '[data-testid="totalEntries_count"]' ).should( 'be.visible' );
@@ -54,6 +54,13 @@ describe( 'Excel File Uploader & Converter', () =>
     } );
     it( 'Upload Excel and Convert to Python [C5]', () =>
     {
+        cy.get( '[data-testid="variable-name-input-field"]' ).should( 'be.visible' ).type( 'pyVariable' ).should( 'have.value', 'pyVariable' );
+        cy.get( 'button[role="combobox"]' ).click();
+        cy.get( 'select' ).select( 'Python', { force: true } );
+        cy.get( 'span[data-slot="select-value"]' ).should( 'have.text', 'Python' );
+        // Upload the Excel file
+        cy.get( 'input[type="file"]' ).selectFile( 'cypress/fixtures/2Game_of_Thrones_Cast_List_with_Earnings.xlsx', { force: true } );
+        cy.get( '[data-testid="totalEntries_count"]' ).should( 'be.visible' );
 
     } );
     it( 'Upload Excel and Convert to Any format and switch [C6]', () =>
